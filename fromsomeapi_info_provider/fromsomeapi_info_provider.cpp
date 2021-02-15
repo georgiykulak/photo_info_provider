c32a19c4c09d8b6885e6e7792bc9a0ef75c44f78
Kulak Georgiy <kulakgeorgij@gmail.com>
2021-02-15 16:34:09 +0200

    Provide C-style getter which is using curl

--- fromsomeapi_info_provider/fromsomeapi_info_provider.cpp
+++ fromsomeapi_info_provider/fromsomeapi_info_provider.cpp
@@ -1,16 +1,40 @@
 // fromsomeapi_info_provider.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#pragma warning(disable : 4996)
+
+#include <stdio.h>
 #include <iostream>
-#define CURL_STATICLIB
-#include <curl\curl.h>
+#include <fstream>
+#include "curl\curl.h"
+using namespace std;
 
-int main()
+
+size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream) {
+	size_t written = fwrite(ptr, size, nmemb, stream);
+	return written;
+}
+
+int main(int n, char* args[])
 {
-    CURL* curl;
+	//BuildBT(10);
+	CURL* curl;
+	CURLcode res;
+	FILE* fp;
+	char outfilename[] = "C:\\Users\\Georgiy\\Projects\\fromsomeapi_info_provider\\out.html";
+	curl = curl_easy_init();
+	if (curl) {
+		fp = fopen(outfilename, "wb");
+		curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.co.uk/");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+		fclose(fp);
+	}
 
-    curl = curl_easy_init();
-    curl_easy_cleanup(curl);
+	cout << "done" << endl;
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu

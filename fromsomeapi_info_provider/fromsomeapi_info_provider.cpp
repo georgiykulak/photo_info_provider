6b0ad5ac911a778c106b88a40a7bd345317845a5
Kulak Georgiy <kulakgeorgij@gmail.com>
2021-02-15 23:15:02 +0200

    Rewrite to much modern C++

--- fromsomeapi_info_provider/fromsomeapi_info_provider.cpp
+++ fromsomeapi_info_provider/fromsomeapi_info_provider.cpp
@@ -1,43 +1,46 @@
-#pragma warning(disable : 4996)
-
-#include <stdio.h>
 #include <iostream>
-#include <fstream>
 #include <curl\curl.h>
 
-size_t write_data( void* ptr, size_t size, size_t nmemb, FILE* stream )
+std::size_t WriteDataCallback( char * in, std::size_t size, std::size_t nmemb, std::string * out )
 {
-	size_t written = fwrite(ptr, size, nmemb, stream);
-	return written;
+	if ( !in )
+		return 0;
+
+	auto writeSize = size * nmemb;
+	out->append( in, writeSize );
+
+	return writeSize;
 }
 
 int main( int argc, char* argv[] )
 {
-	//BuildBT(10);
-	CURL* curl;
-	CURLcode res;
-	FILE* fp;
-	char outfilename[] = "C:\\Users\\Georgiy\\Projects\\fromsomeapi_info_provider\\out.html";
-	curl = curl_easy_init();
-	if (curl) {
-		fp = fopen(outfilename, "wb");
-		
-		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
-		curl_easy_setopt(curl, CURLOPT_URL, "https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=USD&to=UAH&amount=1200&language=en");
+	CURL * curl = curl_easy_init();
 
-		struct curl_slist* headers = NULL;
-		headers = curl_slist_append(headers, "x-rapidapi-key: ffe3711fc7mshb5178c5639ffd04p1bd34ajsn8b932956cd90");
-		headers = curl_slist_append(headers, "x-rapidapi-host: currency-converter5.p.rapidapi.com");
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+	if ( !curl )
+	{
+		std::cout << "Unknown error happened on curl_easy_init()\n\n";
 
-		res = curl_easy_perform(curl);
-		
-		curl_easy_cleanup(curl);
-		fclose(fp);
+		return 1;
 	}
 
-	std::cout << "done" << std::endl;
+	curl_easy_setopt( curl, CURLOPT_CUSTOMREQUEST, "GET" );
+	curl_easy_setopt( curl, CURLOPT_URL, "https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=USD&to=UAH&amount=1200&language=en" );
+
+	struct curl_slist * headers = nullptr;
+	headers = curl_slist_append( headers, "x-rapidapi-key: ffe3711fc7mshb5178c5639ffd04p1bd34ajsn8b932956cd90" );
+	headers = curl_slist_append( headers, "x-rapidapi-host: currency-converter5.p.rapidapi.com" );
+	curl_easy_setopt( curl, CURLOPT_HTTPHEADER, headers );
+	curl_easy_setopt( curl, CURLOPT_WRITEFUNCTION, WriteDataCallback );
+	std::string response;
+	curl_easy_setopt( curl, CURLOPT_WRITEDATA, &response );
+
+	curl_easy_perform( curl );
+		
+	curl_easy_cleanup( curl );
+
+	// TODO: Remove following code
+	std::cout << "done:\n\n";
+	std::cout << "<\n" << response << ">\n\n";
+
 	return 0;
 }
